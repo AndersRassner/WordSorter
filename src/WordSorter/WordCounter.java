9385debdd5638a956ddc2a032a5929320631fb13
Anders Rassner <anders.rassner@dataductus.se>
2020-09-30 07:51:02 +0200

    WordCounter added.
    WordCounter takes an arraylist of words and prints out how many words
    start with each letter of the alphabet.

--- src/WordSorter/WordCounter.java
+++ src/WordSorter/WordCounter.java
@@ -2,26 +2,37 @@ package WordSorter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class WordCounter {
 	private HashMap<Character, Integer> counter;
+	
 	public void countWords(ArrayList<String> filteredWords) {
 		counter = new HashMap<Character, Integer>();
-		Integer value = 0;
-		char[] charArray = IntStream.rangeClosed('a', 'z')
+		char[] alphabet = IntStream.rangeClosed('a', 'z')
 			    .mapToObj(c -> "" + (char) c).collect(Collectors.joining()).toCharArray();
-		for(char c : charArray) {
-			//TODO: implement actual counting
-			value++;
+		
+		for(char c : alphabet) {
+			Integer value = 0;
+			for(String word : filteredWords) {
+				if(word.startsWith("" + c)) {
+					value++;
+				}
+			}
 			counter.put(c, value);
 		}
+		
 		printValues();
 	}
 	
 	private void printValues() {
 		//TODO: replace with printout skipping values of 0
-		System.out.println(counter.toString());
+		for(HashMap.Entry<Character, Integer> pair : counter.entrySet()) {
+			if(pair.getValue() > 0) {
+				System.out.println(pair.getKey() + ": " + pair.getValue());
+			}
+		}
 	}
 }
